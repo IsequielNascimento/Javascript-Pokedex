174321355d3e106aabd21869e96cf98739fbb74a
Isequiel <isequielnascimento@gmail.com>
2022-11-23 11:16:35 -0300

    Adição de detail request e busca automática da lista pokemon

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,13 +2,17 @@
 
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
 
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
     return fetch(url)
         .then((response) => response.json()) //transforma em uma promisse convertida em json
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.error(error))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
-
-
